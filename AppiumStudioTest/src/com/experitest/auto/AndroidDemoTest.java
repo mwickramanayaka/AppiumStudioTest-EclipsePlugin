e55cd3c0e10acd9d004afcb27d80242d72d031a8
mwickramanayaka <manulwick@gmail.com>
2021-06-08 23:55:34 +0530

    Device reflection

--- AppiumStudioTest/src/com/experitest/auto/AndroidDemoTest.java
+++ AppiumStudioTest/src/com/experitest/auto/AndroidDemoTest.java
@@ -12,10 +12,14 @@ public class AndroidDemoTest extends BaseTest {
 	@Parameters("deviceQuery")
 	public void setUp(@Optional("@os='android'") String deviceQuery) throws Exception{
 		init(deviceQuery, "AndroidDemoTest");
+		client.install("cloud:com.medium.reader/com.medium.android.donkey.start.SplashActivity:4.4.1089264", false, false);
+		client.launch("com.medium.reader/com.medium.android.donkey.start.SplashActivity", false, true);
+
 	}
 	
 	@Test
 	public void test(){
+		client.click(in.Repo.zone("LandingPage.Sign_in_"), in.Repo.element("LandingPage.Sign_in_"), 0, 1);
 		
 	}
 	
